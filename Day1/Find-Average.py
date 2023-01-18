e50554d7ac5889c409256a6816dad7a85f05b692
sagacandy <sagarkht@protonmail.com>
2023-01-18 11:12:16 +0545

    iwritw code to find average Please inter the commit message for your changes. Lines starting

---
+++ Day1/Find-Average.py
@@ -0,0 +1,13 @@
+numbers=[]
+while True:
+    input_number=input("Enter the numbers to find average (or 'done' to quit): ")
+    if input_number=='done':
+        break
+    else:
+        numbers.append(int(input_number))
+
+add_numbers=sum(numbers)                #sum of entered numbers
+print('The sum of entered numbers is: ', add_numbers)
+total_numbers=len(numbers)              #total number of terms
+average=add_numbers/total_numbers       #average of entered numbers
+print(f'The average of Entered numbers {numbers} is: ', average)
--- Day1/test.py
+++ Day1/test.py
@@ -18,8 +18,8 @@ while True:
     else:
         numbers.append(int(input_number))
 
-add_numbers=sum(numbers)
+add_numbers=sum(numbers)                #sum of entered numbers
 print('The sum of entered numbers is: ', add_numbers)
-total_numbers=len(numbers)      #total number of terms
-average=add_numbers/total_numbers
+total_numbers=len(numbers)              #total number of terms
+average=add_numbers/total_numbers       #average of entered numbers
 print(f'The average of Entered numbers {numbers} is: ', average)
